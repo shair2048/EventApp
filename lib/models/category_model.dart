1806772c2ba49fd2a84293932d792c959b6cfd6a
Ken <sonhai.it.2048@gmail.com>
2024-07-24 21:11:47 +0700

    category model

---
+++ lib/models/category_model.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class CategoryModel {
+  String name;
+  Color colorBox;
+
+  CategoryModel({
+    required this.name,
+    required this.colorBox,
+  });
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,63 +3,69 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'EventApp',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold
-          ),
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'EventApp',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: GestureDetector(
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: false,
+      // titleSpacing: 10.0,
+      
+      // leading: GestureDetector(
+      //   onTap: () {
+
+      //   },
+      //   child: Container(
+      //     margin: EdgeInsets.all(10),
+      //     alignment: Alignment.center,
+      //     decoration: BoxDecoration(
+      //       color: Color(0xffF7F8F8),
+      //       borderRadius: BorderRadius.circular(10)
+      //     ),
+      //     child: SvgPicture.asset(
+      //       'assets/icons/arrow_left.svg',
+      //       height: 20,
+      //       width: 20,
+      //     ),
+      //   ),
+      // ),
+      actions: [
+        GestureDetector(
           onTap: () {
 
           },
           child: Container(
             margin: EdgeInsets.all(10),
+            width: 37,
             alignment: Alignment.center,
             decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
+              color: Colors.white,
               borderRadius: BorderRadius.circular(10)
             ),
             child: SvgPicture.asset(
-              'assets/icons/arrow_left.svg',
+              'assets/icons/notification.svg',
               height: 20,
               width: 20,
             ),
           ),
-        ),
-        actions: [
-          GestureDetector(
-            onTap: () {
-
-            },
-            child: Container(
-              margin: EdgeInsets.all(10),
-              width: 37,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/notification.svg',
-                height: 20,
-                width: 20,
-              ),
-            ),
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
